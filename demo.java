7fb7215eb0eef7ce042ac119d61f530980ee5886
wwtdsg <wwtdsg@gmail.com>
2015-02-07 10:42:47 +0800

    first blood

---
+++ demo.java
@@ -0,0 +1,16 @@
+/**this is a test Demo for java
+ * version 0.1
+ * author wangtao
+ */
+
+import javax.swing.*;
+class Ch2Sample1
+{
+    public static void main (String[] args){
+        JFrame myWindow = new JFrame();
+        myWindow.setSize(300,200);
+        myWindow.setTitle("My First Java Program");
+        myWindow.setVisible(true);
+        myWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
